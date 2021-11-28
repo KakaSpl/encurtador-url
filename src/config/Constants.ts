8734eda5adee85f32ded359949dbe9d00b28199d
Carlos Eduardo Martins <79061113+KakaSpl@users.noreply.github.com>
2021-11-27 23:51:27 -0300

    Add files via upload

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:3000',
+	MONGO_CONNECTION:
+	'mongodb+srv://kakaspl:14seven1986@url-short-dio.ohdwe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority,'
+}
